f4c6aaa73e22eb2e499ff22d5f17e58640e46efe
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-07-06 16:10:54 +0200

    💄✨ oppdaterte stil på feilinnmeldings-siden og la til routing tilbake til hovedsiden
    Co-authored-by: Sindre Kjelsrud <sindre.kjelsrud@nav.no>

--- frontend/pages/feil/index.tsx
+++ frontend/pages/feil/index.tsx
@@ -4,6 +4,7 @@ import { ArrowLeftIcon, BugIcon, UploadIcon } from "@navikt/aksel-icons";
 import { Button, Heading, TextField, Textarea } from "@navikt/ds-react";
 import post from "../api/http";
 import { useState } from "react";
+import router from "next/router";
 
 export default function Feil() {
     const [tittel, setTittel] = useState("");
@@ -21,52 +22,59 @@ export default function Feil() {
     }
 
     return (
-        <main className="flex flex-col justify-center gap-32 p-16">
-            <div className="w-1/2 flex flex-col gap-4 justify-center">
-                <BugIcon
-                    title="Insekts ikon"
-                    fontSize="3rem"
-                />
-                <Heading
-                    level="1"
-                    size="xlarge"
-                >
-                    Innmelding av feil i speil
-                </Heading>
-            </div>
-            <div className="w-1/2 flex flex-col gap-4 justify-center">
-                <TextField
-                    label="Tittel"
-                    description="En kort oppsummering av problemet"
-                    onChange={e => setTittel(e.target.value)}
-                />
-                <Textarea
-                    label="Beskrivelse"
-                    description="Detaljert beskrivelse av problemet"
-                    onChange={e => setBeskrivelse(e.target.value)}
-                />
+        <main className="flex h-screen">
+            <div className="w-1/4 bg-bg-subtle"></div>
+            <div className="flex flex-col justify-center gap-32 p-16 grow">
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <BugIcon
+                        title="Insekts ikon"
+                        fontSize="3rem"
+                    />
+                    <Heading
+                        level="1"
+                        size="xlarge"
+                    >
+                        Innmelding av feil i speil
+                    </Heading>
+                </div>
+                <div className="w-1/2 flex flex-col gap-4 justify-center">
+                    <TextField
+                        label="Tittel"
+                        description="En kort oppsummering av problemet"
+                        onChange={e => setTittel(e.target.value)}
+                    />
+                    <Textarea
+                        label="Beskrivelse"
+                        description="Detaljert beskrivelse av problemet"
+                        onChange={e => setBeskrivelse(e.target.value)}
+                    />
 
-                <Button
-                    variant="secondary"
-                    icon={<UploadIcon />}
-                >
-                    Last opp skjermbilder
-                </Button>
+                    <Button
+                        variant="secondary"
+                        icon={<UploadIcon />}
+                    >
+                        Last opp skjermbilder
+                    </Button>
+                </div>
+                <div className="w-1/2 flex flex-col gap-2 justify-center">
+                    <Button
+                        onClick={handleSubmit}
+                        variant="primary"
+                    >
+                        Meld inn feil
+                    </Button>
+                    <Button
+                        variant="tertiary"
+                        icon={<ArrowLeftIcon />}
+                        onClick={() => {
+                            router.push('/')
+                        }}
+                    >
+                        Gå tilbake til hovedmenyen
+                    </Button>
+                </div>                
             </div>
-            <div className="w-1/2 flex flex-col gap-2 justify-center">
-                <Button
-                    onClick={handleSubmit}
-                    variant="primary"
-                >
-                    Meld inn feil
-                </Button>
-                <Button
-                    variant="tertiary"
-                    icon={<ArrowLeftIcon />}
-                >
-                    Gå tilbake til hovedmenyen
-                </Button>
-            </div>
+            <div className="w-1/4 bg-bg-subtle"></div>
         </main>
     )
 }

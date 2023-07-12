889e7b2cd2850997d495f91f4c4e571ec5fdf0b2
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-07-12 17:58:24 +0200

    ✨ Laget frontend ikke-funksjonellt skjermbildeOpplastning-komponent

--- frontend/pages/feil/index.tsx
+++ frontend/pages/feil/index.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import router from "next/router";
 import BildeOpplastning from "@/components/BildeOpplastning";
 import Header from "@/components/Header";
+import BildeOpplastning from "@/components/BildeOpplastning";
 
 export default function Feil() {
     const [tittel, setTittel] = useState("");

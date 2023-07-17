a5497cf536ef801844d0028e15b7b487f7c9356a
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-07-17 15:40:26 +0200

    🥅 Sørger for rett destinasjon for HTTP requests fra frontend
    Co-authored-by: Sindre Kjelsrud <sindre.kjelsrud@nav.no>
    Co-authored-by: Amalie Mansåker <amalie.erdal.mansaker@nav.no>

--- frontend/pages/api/http.ts
+++ frontend/pages/api/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default function post(route: String, data?: Object) {
-    axios.post("http://0.0.0.0:8080/" + route, data, {
+    axios.post("http://helse-sprik.intern.dev.nav.no" + route, data, {
         headers: {
             'Content-Type': 'application/json'
         }

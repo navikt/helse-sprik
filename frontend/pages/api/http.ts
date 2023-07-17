0344e3cc0eab83d458850f45acd361da056dc975
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-07-17 16:18:39 +0200

    🚚 Https over http i post route for å unngå konflikt med backend
    Co-authored-by: Sindre Kjelsrud <sindre.kjelsrud@nav.no>
    Co-authored-by: Amalie Mansåker <amalie.erdal.mansaker@nav.no>

--- frontend/pages/api/http.ts
+++ frontend/pages/api/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default function post(route: String, data?: Object) {
-    axios.post("http://helse-sprik.intern.dev.nav.no" + route, data, {
+    axios.post("https://helse-sprik.intern.dev.nav.no" + route, data, {
         headers: {
             'Content-Type': 'application/json'
         }

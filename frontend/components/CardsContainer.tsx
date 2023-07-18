c6e4c9fd0089f352b796d62e9d8a8966bfea03a1
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-07-18 10:36:22 +0200

    ♻️  Dynamisk innlastning av feilinnmeldingsobjekter til frontend grid

--- frontend/components/CardsContainer.tsx
+++ frontend/components/CardsContainer.tsx
@@ -1,18 +1,44 @@
 import FeilCard from "./FeilCard";
 
-
+type FeilMelding = {
+    tittel: String
+    beskrivelse: String
+}
 /**
  * 
  * @returns Komponent for returnering av konteiner med alle feilinnmeldingene. 
  */
 const CardsContainer = () => {
+    
+    const feilMeldinger: FeilMelding[] = [
+        {
+            tittel: "Mangel på hensyn til tariffoppgjør",
+            beskrivelse: "Det har vært ett tariffoppgjør og speil sier sykepengene må tilbakekreves, noe som er feil. (sier vi.. har ikke domenekunnskap)"
+        },
+        {
+            tittel: "Speil sier NAV må tilbakekreve sykepenger på feil grunnlag",
+            beskrivelse: "baluba"
+        },
+        {
+            tittel: "Feil A",
+            beskrivelse: "Lorem Ipsum"
+        },
+        {
+            tittel: "Feil B",
+            beskrivelse: "dolor"
+        },
+        {
+            tittel: "Håndkle på hue",
+            beskrivelse: "Bombastic side eye"
+        }
+    ]
+
     return(
         <div>
             <div className="grid grid-cols-2 gap-4">
-                <FeilCard tittel="Mangel på hensyn til tariffoppgjør" beskrivelse="Det har vært ett tariffoppgjør og speil sier sykepengene må tilbakekreves, noe som er feil. (sier vi.. har ikke domenekunnskap)"/>  
-                <FeilCard tittel="Speil sier NAV må tilbakekreve sykepenger på feil grunnlag" beskrivelse="baluba"/>  
-                <FeilCard tittel="Feil A" beskrivelse="Dette er Feil fordi x, y, z"/>  
-                <FeilCard tittel="Feil B" beskrivelse="Lorem Ipsum"/>  
+                {feilMeldinger.map((feilMelding) => (
+                    <FeilCard tittel={feilMelding.tittel} beskrivelse={feilMelding.beskrivelse}/> 
+                ))}
             </div>     
         </div>
 

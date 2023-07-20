baf3e3a867cb31a254467067913f71346d0569eb
Sindre Kjelsrud <sindre.kjelsrud@nav.no>
2023-07-20 16:10:10 +0200

    ⚰️  Fjernet ubrukt kode for å fikse deploy
    Co-authored-by: Markus A. R. Johansen <markus.aleksander.rakil.johansen@nav.no>

--- backend/src/test/kotlin/LocalApp.kt
+++ backend/src/test/kotlin/LocalApp.kt
@@ -1,12 +1,11 @@
 import no.nav.helse.sprik.Application
 import no.nav.helse.sprik.configureFlyway
 import no.nav.helse.sprik.db.Database
-import no.nav.helse.sprik.db.FeilmeldingRepository
+// import no.nav.helse.sprik.db.FeilmeldingRepository
 import no.nav.helse.sprik.dbconfig
-import no.nav.helse.sprik.modell.Feilmelding
-import org.intellij.lang.annotations.Language
-import java.time.LocalDateTime
-import javax.sql.DataSource
+// import no.nav.helse.sprik.modell.Feilmelding
+// import java.time.LocalDateTime
+// import javax.sql.DataSource
 
 
 fun main() {
@@ -14,12 +13,14 @@ fun main() {
     val db = Database(dbconfig()).configureFlyway()
     val app = Application(db)
 
-    oppretteMockData(db.dataSource)
+    // oppretteMockData(db.dataSource) // WIP
 
     app.startBlocking()
 }
 
+/* WIP
 private fun oppretteMockData(dataSource: DataSource) {
     val feilmeldingRepository = FeilmeldingRepository()
     feilmeldingRepository.lagre(Feilmelding("Tittel Test", "Beskrivelse Test", LocalDateTime.of(2023, 1, 1, 8, 0, 0)))
 }
+*/
--- frontend/src/components/CardsContainer.tsx
+++ frontend/src/components/CardsContainer.tsx
@@ -1,4 +1,3 @@
-import axios from "axios"
 import FeilCard from "./FeilCard"
 import useSWR from "swr"
 /**

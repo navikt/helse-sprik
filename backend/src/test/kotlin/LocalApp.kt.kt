f98dde2b44021f562b7e50515997389525d7c02a
Sindre Kjelsrud <sindre.kjelsrud@nav.no>
2023-07-20 15:57:51 +0200

    🚧 Mockdata lokalt in progresso
    Co-authored-by: Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>

--- backend/src/test/kotlin/LocalApp.kt.kt
+++ backend/src/test/kotlin/LocalApp.kt.kt
@@ -1,7 +1,12 @@
 import no.nav.helse.sprik.Application
 import no.nav.helse.sprik.configureFlyway
 import no.nav.helse.sprik.db.Database
+import no.nav.helse.sprik.db.FeilmeldingRepository
 import no.nav.helse.sprik.dbconfig
+import no.nav.helse.sprik.modell.Feilmelding
+import org.intellij.lang.annotations.Language
+import java.time.LocalDateTime
+import javax.sql.DataSource
 
 
 fun main() {
@@ -9,5 +14,12 @@ fun main() {
     val db = Database(dbconfig()).configureFlyway()
     val app = Application(db)
 
+    oppretteMockData(db.dataSource)
+
     app.startBlocking()
+}
+
+private fun oppretteMockData(dataSource: DataSource) {
+    val feilmeldingRepository = FeilmeldingRepository()
+    feilmeldingRepository.lagre(Feilmelding("Tittel Test", "Beskrivelse Test", LocalDateTime.of(2023, 1, 1, 8, 0, 0)))
 }

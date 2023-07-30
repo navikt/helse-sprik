545d64da8c31fb75732f8b5101d93168f8ac4d71
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-07-30 18:04:47 +0200

    🐛 La tilbake interface, da fjerning av dette brekket applikasjon

--- frontend/src/components/CardsContainer.tsx
+++ frontend/src/components/CardsContainer.tsx
@@ -1,17 +1,21 @@
 import { FeilKort } from "./FeilCard";
 import { Feilmelding } from "../interface.ts";
 
+interface ICardsContainer {
+  feilmeldinger: Feilmelding[]
+}
+
 /**
  * Komponent som laster inn feilmeldinger i kort fra database.
  * @returns grid med feilmeldinger 
  */
-const CardsContainer = (feilmeldinger: Feilmelding[]) => {
+const CardsContainer = (props: ICardsContainer) => {
   return (
     <div>
       <div className="grid grid-cols-2 gap-6">
-        {feilmeldinger.map((feilMelding) => (
+        {props.feilmeldinger.map((feilMelding) => (
           <FeilKort
-            key={feilmeldinger.indexOf(feilMelding)}
+            key={props.feilmeldinger.indexOf(feilMelding)}
             tittel={feilMelding.tittel}
             beskrivelse={feilMelding.beskrivelse}
             dato={new Date()}

561bd3c866d6244fbbecbea870273937aa24a5f1
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-07-30 17:54:11 +0200

    ⚰️  Forenklet cardscontainer komponentet ved å fjerne interface

--- frontend/src/components/CardsContainer.tsx
+++ frontend/src/components/CardsContainer.tsx
@@ -1,29 +1,17 @@
 import { FeilKort } from "./FeilCard";
 import { Feilmelding } from "../interface.ts";
 
-interface ICardsContainer {
-  feilmeldinger: Feilmelding[]
-}
-
 /**
  * Komponent som laster inn feilmeldinger i kort fra database.
  * @returns grid med feilmeldinger 
  */
-const CardsContainer = (props: ICardsContainer) => {
+const CardsContainer = (feilmeldinger: Feilmelding[]) => {
   return (
     <div>
       <div className="grid grid-cols-2 gap-6">
-        {/* {props.feilmeldinger.map((feilMelding) => (
-          <FeilCard1
-            key={props.feilmeldinger.indexOf(feilMelding)}
-            tittel={feilMelding.tittel}
-            beskrivelse={feilMelding.beskrivelse}
-            dato={new Date()}
-          />
-        ))} */}
-        {props.feilmeldinger.map((feilMelding) => (
+        {feilmeldinger.map((feilMelding) => (
           <FeilKort
-            key={props.feilmeldinger.indexOf(feilMelding)}
+            key={feilmeldinger.indexOf(feilMelding)}
             tittel={feilMelding.tittel}
             beskrivelse={feilMelding.beskrivelse}
             dato={new Date()}

8091b21b20d3e9122399e5325365d58e61793bc1
Markus Johansen <markus.aleksander.rakil.johansen@nav.no>
2023-08-07 12:00:51 +0200

    📝 Dokumentert redigeringsverktøy, skillelinje og tagbar

--- frontend/src/components/Skillelinje.tsx
+++ frontend/src/components/Skillelinje.tsx
@@ -1,5 +1,8 @@
 
-
+/**
+ * Skillelinjer som brukes for å dele opp innholdet i et komponent/skjema i mindre deler
+ * Forbedrer lesbarheten til skjemaet
+ */
 const Skillelinje = () => {
     return (
         <div className="h-1 bg-gray-200 my-3 rounded-lg w-full"></div>
--- frontend/src/components/TagBar.tsx
+++ frontend/src/components/TagBar.tsx
@@ -4,33 +4,31 @@ interface TagBarInterface {
     haster: boolean
     arbeidsstatus: number
 }
+
+/**
+ * @param arbeidsstatus er en kode som beskriver om feilen er fikset, jobbes med eller ikke påbegynt. må være 0, 1 eller 2.
+ * @returns Tag komponent med riktig farge og tekst basert på arbeidsstatus-kode
+ */
+const toggleArbeidsstatus = (arbeidsstatus: number) => {
+    switch (arbeidsstatus) {
+        case 0:
+            return <Tag variant="neutral">Ikke påbegynt</Tag>;
+        case 1:
+            return <Tag variant="info">Feilen jobbes med</Tag>;
+        case 2:
+            return <Tag variant="success">Feilen er fikset</Tag>;
+        default:
+            throw new Error("Ikke gyldig arbeidsstatus-kode. Koden må være 0, 1 eller 2");
+    }
+}
+                
 /**
  * Komponentet er en bar (vanrett linje) som inneholder to statusflagg: "arbeidsstatus" og "Haster".
  * Komponentet er en del av FeilKortHeader
  * @param haster er en boolean som beskriver om feilen haster eller ikke.
- * @param arbeidsstatus
+ * @param arbeidsstatus er en kode som beskriver om feilen er fikset, jobbes med eller ikke påbegynt. må være 0, 1 eller 2.
  */
 const TagBar = (props: TagBarInterface) => {
-
-    /**
-     * Funksjonen tar inn 
-     * @param arbeidsstatus 
-     * @returns 
-     */
-    const toggleArbeidsstatus = (arbeidsstatus: number) => {
-        switch (arbeidsstatus) {
-            case 0:
-                return <Tag variant="neutral">Ikke påbegynt</Tag>;
-            case 1:
-                return <Tag variant="info">Feilen jobbes med</Tag>;
-            case 2:
-                return <Tag variant="success">Feilen er fikset</Tag>;
-            default:
-                throw new Error("Ikke gyldig arbeidsstatus-kode. Koden må være 0, 1 eller 2");
-                
-        }
-    }
-
     return (
         <div className="flex gap-3 mt-4">
             {toggleArbeidsstatus(props.arbeidsstatus)}
